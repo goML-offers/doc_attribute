a74b485daee4a1bd27c318174a050d497ac4627c
varshamani13 <gokulavarshini.m@goml.io>
2023-12-07 14:05:28 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
+from typing import List
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -30,36 +32,30 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENVIRONMENT = os.getenv('PINECONE_ENVIRONMENT')
 
+class AttributeInput(BaseModel):
+    formatted_prompt: str
 
 def vectorize_and_store_documents(pdf_filename):
-
-
     pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
-
     print('Checking if index exists...')
     
-    
 
+    if PINECONE_INDEX_NAME in pinecone.list_indexes():
+        print("deleting the existing index")
+        pinecone.delete_index(PINECONE_INDEX_NAME)
+        print("deleted")
 
-    if PINECONE_INDEX_NAME not in pinecone.list_indexes():
+    time.sleep(20)
 
-        
+    if PINECONE_INDEX_NAME not in pinecone.list_indexes(): 
         print('Index does not exist, creating index...')
         # we create a new index
-        pinecone.create_index(
-            name=PINECONE_INDEX_NAME,
-            metric='cosine',
-            # The OpenAI embedding model `text-embedding-ada-002 uses 1536 dimensions`
-            dimension=1536
-        )
-
+        pinecone.create_index(name=PINECONE_INDEX_NAME,metric='cosine',dimension=1536)
         #pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
-        
 
     # Load PDF documents
     loader = PyPDFLoader(pdf_filename)
     pages = loader.load()
-
     # Chunk data into smaller documents
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000, chunk_overlap=100)
@@ -71,36 +67,36 @@ def vectorize_and_store_documents(pdf_filename):
     docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=PINECONE_INDEX_NAME)
     print(docsearch)
     time.sleep(20)
-    questions = [
-        "Provide me the Insured Name. Do not add extra wordings. The name is given in the document. Check it iteratively and respond precisely. For example:Gokula Varshini Manikandan",
-        "Provide me the Policy Number given in the document. Do not add extra wordings.Check it iteratively and respond precisely. For example:TIT/92033282",
-        "Provide me the policy period . Do not add extra wordings.Check it iteratively and respond precisely. For Example: 29-APR-2022 to Midnight of 28-APR-2023",
-        "Which company has provided the policy? Extract me only the company name: <<company name>>. Do not add extra wordings. For example: Third Party Plan"]
-    # questions=["Provide me the Policy Number"]
-    
-    llm_chat = ChatOpenAI(temperature=0.5, max_tokens=50,
-                         model='gpt-3.5-turbo-0613', client='')
 
+
+
+def define_prompt(formatted_prompt):
+    llm_chat = ChatOpenAI(temperature=0.5, max_tokens=50, model='gpt-3.5-turbo-0613', client='')
     embeddings = OpenAIEmbeddings(client='')
-    #Set Pinecone index
-    docsearch = Pinecone.from_existing_index(
-        index_name=PINECONE_INDEX_NAME, embedding=embeddings)
-    # Create chain
+    docsearch = Pinecone.from_existing_index(index_name=PINECONE_INDEX_NAME, embedding=embeddings)
     chain = load_qa_chain(llm_chat)
-    
-    print("=========================")
-    print(docsearch)
-    print("==========================")
     answers = []
-    for question in questions:
-        search = docsearch.similarity_search(question)
+    attributes=[attr.strip() for attr in formatted_prompt.split(",")]
+
+
+    for attribute in attributes:
+        question=f"Provide me the {attribute}. Do not add extra wordings.Check it iteratively and respond precisely. Add the confidence scale from scale 0 to 1.Provide me in two line For example: policy period :'20 Apr 23 00:00 hrs to 19 Apr 25 23:59 hrs'. \n Confidence level: '0.9'"
+        search = docsearch.similarity_search(attribute)
         response = chain.run(input_documents=search, question=question)
+        print("============")
         print(response)
-        answers.append(response)
-    print("empty++++++++++++++",docsearch)
+        print("=============")
+        
+        lines = response.strip().split('\n')
+        if len(lines) >= 2:
+            attribute_value = lines[0].split(':',1)[1].strip()
+            confidence_value = lines[1].split(':')[1].strip()
+            answers.append({attribute: attribute_value, "confidence": float(confidence_value)})
+        else:
+            answers.append({attribute: "N/A", "confidence": 0.0})  # Handle cases where lines are not present
+        # answers.append({"attribute":attribute})
     
     print(answers)
-    
     return answers
 
 @app.post("/upload-policy/")
@@ -108,31 +104,16 @@ async def upload_policy(pdf_file: UploadFile = File(...)):
     with open(pdf_file.filename, "wb") as f:
         f.write(pdf_file.file.read())
     
-    answers=vectorize_and_store_documents(pdf_file.filename)
+    vectorize_and_store_documents(pdf_file.filename)
     os.remove(pdf_file.filename)
-     
-    # answers=extract_metadata()
-    
-    
-    excel_filename = "answers_output.xlsx"
-    if os.path.exists(excel_filename):
-        existing_df = pd.read_excel(excel_filename)
-    else:
-        existing_df = pd.DataFrame(columns=["policy_holder_name", "policy_number", "policy_period", "policy_name"])
-    new_data = pd.DataFrame([answers], columns=existing_df.columns)
-    updated_df = pd.concat([existing_df, new_data], ignore_index=True)
-    updated_df.to_excel(excel_filename, index=False)
-    
-    
-    # index.delete(delete_all=True, namespace='gcp-starter')
-    if PINECONE_INDEX_NAME in pinecone.list_indexes():
-        print("deleting the existing index")
-        pinecone.delete_index(PINECONE_INDEX_NAME)
-        print("deleted")
 
-    return {"message": "Answers extracted, Pinecone index deleted, and appended to Excel.", "file_name": excel_filename}
+@app.post("/attributes/")
+async def attributes(input_data: AttributeInput):
+    formatted_prompt = input_data.formatted_prompt
+    answers = define_prompt(formatted_prompt)
+    return answers
 
-
+    # return {"message": "Answers extracted, Pinecone index deleted, and appended to Excel.", "file_name": excel_filename}
 # Run the FastAPI app
 if __name__ == "__main__":
     import uvicorn
